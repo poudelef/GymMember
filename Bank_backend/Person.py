85d1ad02bf9c0325624bbcf45384546d74d65d52
sambhavpoudel88@gmail.com <youremail@example.com>
2025-08-12 20:52:51 -0400

    Updated Person

--- Bank_backend/Person.py
+++ Bank_backend/Person.py
@@ -1,7 +1,7 @@
 import datetime
 class Person:
     __count = 0 # class variabe aka static field
-    __DOB_count = 1
+    
     __MAX_DOB_UPDATES = 3
     __registered_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") 
     def __init__(self, name = "", __DOB = "", phoneNumber = "", gender = "", address = "", ): # __init__ is a constructor in python
@@ -12,20 +12,21 @@ class Person:
         self.gender = gender
         self.address = address
         self.__registered_date = Person.__registered_date
+        self.__DOB_count = 1
     
 
     def update_DOB(self, new_DOB):
-        if Person.__DOB_count > Person.__MAX_DOB_UPDATES:
-            raise Exception("You already updated your DOB 3 times, you can't update it anymore. Please contact support if you need to update it again.")
+        if self.__DOB_count > Person.__MAX_DOB_UPDATES:
+            raise PermissionError("You already updated your DOB 3 times, you can't update it anymore. Please contact support if you need to update it again.")
             return False
         else:
             self.DOB = new_DOB
-            if Person.__DOB_count - Person.__MAX_DOB_UPDATES == 0:
+            if self.__DOB_count - Person.__MAX_DOB_UPDATES == 0:
                 print("You can't update your DOB anymore")
             else:    
-                print(f"DOB Updated Successfully, You can update it {Person.__MAX_DOB_UPDATES-Person.__DOB_count} more times")
+                print(f"DOB Updated Successfully, You can update it {Person.__MAX_DOB_UPDATES-self.__DOB_count} more times")
 
-            Person.__DOB_count += 1
+            self.__DOB_count += 1
             return True
     
     def calculate_age(self):
@@ -46,13 +47,13 @@ class Person:
             }
     
 
-Sambhav = Person("Sambhav", "2005-01-31", "513-276-2979", "Male", "1234 Elm St, Springfield, IL")    
-print(Sambhav.person_info())
+# Sambhav = Person("Sambhav", "2005-01-31", "513-276-2979", "Male", "1234 Elm St, Springfield, IL")    
+# print(Sambhav.person_info())
 
-Sambhav.update_DOB("2005-02-01")
-Sambhav.update_DOB("2005-02-02")
-Sambhav.update_DOB("2005-02-03")
-Sambhav.update_DOB("2005-02-04")
-# # # # Sambhav.update_DOB("2005-02-05")
+# Sambhav.update_DOB("2005-02-01")
+# Sambhav.update_DOB("2005-02-02")
+# Sambhav.update_DOB("2005-02-03")
+# Sambhav.update_DOB("2005-02-04")
+# Sambhav.update_DOB("2005-02-05")
 
 
